6cca6e6f291abc45b874457e59667307f096c448
Cameron Emery <75042648+mESmaC@users.noreply.github.com>
2024-11-27 18:49:52 -0500

    Bubb sort and metrics

---
+++ algs.net/Sorting/BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿namespace algs.net.Sorting {
+    public class BubbleSort {
+        public static void Sort<T>(T[] array) where T : IComparable<T> {
+            int len = array.Length;
+            bool swap;
+
+            for (int i = 0; i < len - 1; i++) {
+                swap = false;
+                for (int j = 0; j < len - i - 1; j++) {
+                    if (array[j].CompareTo(array[j + 1]) > 0) {
+                        T temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swap = true;
+                    }
+                }
+                if (!swap) {
+                    break;
+                }
+            }
+        }
+    }
+}
