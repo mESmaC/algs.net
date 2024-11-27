ce3ec224b346a0f649bde70635a8428fd9f78a61
Cameron Emery <75042648+mESmaC@users.noreply.github.com>
2024-11-27 16:21:17 -0500

    Added metrics to view execution time of algorithms.

---
+++ algs.net/Utilities/Metrics.cs
@@ -0,0 +1,21 @@
+﻿namespace algs.net.Utilities {
+    using System;
+    using System.Diagnostics;
+
+    public static class Metrics {
+        /// <summary>
+        /// Measures the execution time of a provided action.
+        /// </summary>
+        /// <param name="algorithmName">The name of the algorithm being measured.</param>
+        /// <param name="action">The algorithm to be executed.</param>
+        public static void MeasureExecutionTime(string algorithmName, Action action) {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            action();
+
+            stopwatch.Stop();
+            Console.WriteLine($"{algorithmName} Execution Time: {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
