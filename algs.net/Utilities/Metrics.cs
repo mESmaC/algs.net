dd08c9a9d79482fdf0b026dba5feaf5d88083c5f
Cameron Emery <75042648+mESmaC@users.noreply.github.com>
2024-11-27 18:46:37 -0500

    Added memory tracking to metrics. And added bubble sort

--- algs.net/Utilities/Metrics.cs
+++ algs.net/Utilities/Metrics.cs
@@ -4,18 +4,26 @@
 
     public static class Metrics {
         /// <summary>
-        /// Measures the execution time of a provided action.
+        /// Measures the execution time and memory usage of a provided action.
         /// </summary>
         /// <param name="algorithmName">The name of the algorithm being measured.</param>
         /// <param name="action">The algorithm to be executed.</param>
-        public static void MeasureExecutionTime(string algorithmName, Action action) {
+        public static void MeasureExecutionTimeAndMemory(string algorithmName, Action action) {
+            long memoryBefore = GC.GetTotalMemory(forceFullCollection: true);
+
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
             action();
 
             stopwatch.Stop();
+
+            long memoryAfter = GC.GetTotalMemory(forceFullCollection: true);
+
+            long memoryUsed = memoryAfter - memoryBefore;
+
             Console.WriteLine($"{algorithmName} Execution Time: {stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"{algorithmName} Memory Usage: {memoryUsed / 1024.0:F2} KB");
         }
     }
 }
