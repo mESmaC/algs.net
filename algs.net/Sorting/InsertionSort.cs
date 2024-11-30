17caa764806b70d641202792d2d7f5267626fb57
Cameron Emery <75042648+mESmaC@users.noreply.github.com>
2024-11-30 00:55:26 -0500

    Added InsertionSort

---
+++ algs.net/Sorting/InsertionSort.cs
@@ -0,0 +1,18 @@
+﻿namespace algs.net.Sorting {
+    public class InsertionSort {
+        public static void Sort<T>(T[] array) where T : IComparable<T> {
+            int n = array.Length;
+
+            for (int i = 1; i < n; ++i) {
+                T key = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j].CompareTo(key) > 0) {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j] = key;
+            }
+        }
+    }
+}
